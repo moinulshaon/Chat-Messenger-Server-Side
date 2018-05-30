be686851b41078f5ceba782d5cc6b72ad5a16d77
Moinul Hasan Shaon <moinul.shaon@gmail.com>
2018-05-31 01:10:36 +0800

    bipassed cors problem

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
-insert into user (user_id, user_name) values(0, 'Shaon');
+insert into user (user_id, user_name) values(0, 'Sheikh');
 insert into user (user_id, user_name) values(1, 'Moinul');
+insert into user (user_id, user_name) values(2, 'Hasan');
